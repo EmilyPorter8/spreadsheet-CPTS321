58e0dddc237ba97a60dbd0e0416725bb536c40bf
EmilyPorter84 <emilyporter79@gmail.com>
2024-09-04 11:15:19 -0700

    Get list of input from user, created files for BST

---
+++ HW1/HW1/BST.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1
+{
+    public class BST
+    {
+        protected Node root;
+        public BST() { }
+        public BST(string userInput) 
+        {
+
+        }
+    }
+}
---
+++ HW1/HW1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace HW1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("BST IMPLEMENTATION");
+            Console.WriteLine("\nPlease enter list of integers:");
+            string userList = Console.ReadLine();
+
+            //call BST
+        }
+    }
+}
